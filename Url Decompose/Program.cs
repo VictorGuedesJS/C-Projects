4c42599c5a8201c4c760f30ccca0a02f8d3135fa
VictorGuedesJS <victorguedes.js@gmail.com>
2024-06-10 15:20:15 -0400

    Adiconando projeto:
    Validação de email
    
    decompor URL
    
    contação de votos

---
+++ Url Decompose/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Url_Decompose;
+
+class Program
+{
+        public static void Main(string[] args)
+        {
+            // MARK: Setup
+            string exampleURL = "www.docs.microsoft.com/dotnet/csharp/whats-new/csharp-version-history";
+            Console.WriteLine($"Hit ENTER to break down the URL into its component parts: {exampleURL}");
+
+            // MARK: Result
+            var components = BreakdownURL(exampleURL);
+            for (int i = 0; i < components.Length; i++)
+            {
+                var indent = new string(' ', i * 2);
+                Console.WriteLine($"{indent} -> {components[i]}");
+            }
+        }
+
+        // MARK: Calculation
+        public static string[] BreakdownURL(string urlString)
+        {
+           urlString = urlString.Replace("www.", "");
+           urlString = urlString.Replace("-", " ");
+
+           string[] urlNova = urlString.Split("/");
+
+
+
+            return urlNova;
+        }
+
+}
---
+++ Voting/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Voting;
+
+class Program
+{
+class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // MARK: Setup
+            Console.WriteLine("How old are you?");
+            int input = int.Parse(Console.ReadLine());
+
+            // MARK: Result
+            var presidents = CalculatePresidents(input);
+            Console.WriteLine($"You've voted for {presidents} presidents!");
+        }
+
+        // MARK: Write your solution here
+        public static int CalculatePresidents(int age)
+        {
+            if (age < 18)
+            return 0;
+            else
+                return (age - 18) / 4;
+        }
+    }
+}

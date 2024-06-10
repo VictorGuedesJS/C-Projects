684340643b12a70f28e35afea57864d2ae06198a
VictorGuedesJS <victorguedes.js@gmail.com>
2024-06-10 15:20:39 -0400

    joreg

---
+++ email validation/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace email_validation;
+
+class Program
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // MARK: Setup
+            Console.WriteLine("Enter the email address you'd like to validate:");
+            var input = Console.ReadLine();
+
+            // MARK: Result
+            ValidateEmail(input);
+
+        }
+
+        // MARK: Write your solution here
+        public static void ValidateEmail(string email)
+        {
+            if (!char.IsLetter(email.First()))
+
+                Console.WriteLine("This email is invalid");    
+
+            else{
+
+                if (email.Contains("@"))
+                {
+                    if (email.EndsWith(".com"))
+                    {
+                        Console.WriteLine("This is email is valid");
+                    }
+                    else{
+                        Console.WriteLine("This email is invalid");
+                    }
+                    
+                }else{
+                    Console.WriteLine("This email is invalid");
+                }
+                  
+            }
+
+        }
+    }
+}
